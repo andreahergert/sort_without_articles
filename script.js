46b9a86b181f3a3e26a859c97db1042f58abeb45
Andrea Hergert <ahergert24@hotmail.com>
2023-03-19 20:01:19 -0700

    added functionality and screenshot

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+const bandNames = bands.sort(function (a, b) {
+    return strip(a) > strip(b) ? 1 : -1;
+    // or use below
+    // if(strip(a) > strip(b)) {
+    //     return 1;
+    // } else {
+    //     return -1;
+    // }
+});
+
+document.querySelector('#bands').innerHTML = 
+    bandNames
+        .map(band => `<li>${band}</li>`)
+        .join('');
